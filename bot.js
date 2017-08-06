b509caad163b1f00a9abf98757ccd2771e1fce22
David.Hahn <ddavidhahn@gmail.com>
2017-08-05 22:21:37 -0700

    Faulty playback

--- bot.js
+++ bot.js
@@ -1,17 +1,18 @@
+var auth = require('./auth.json');
 var Discord = require('discord.io');
 var logger = require('winston');
-var auth = require('./auth.json');
-var settings = require('./settings.js');
 var nano = require('nano')('http://localhost:5984');
+var settings = require('./settings.js');
+var ytaStream = require('youtube-audio-stream');
 
 // Configure couchdb
-nano.db.get('bach-bot-db', function(err, body) {
-    if (!err) {
+nano.db.get('bach-bot-db', function(error, body) {
+    if (!error) {
         logger.info('bach-bot-db already exists. Continuing execution...');
     } else {
         logger.info('bach-bot-db does not exist. Creating it...');
-        nano.db.create('bach-bot-db', function(err, body) {
-            if (!err) {
+        nano.db.create('bach-bot-db', function(error, body) {
+            if (!error) {
                 logger.info('Database bach-bot-db created!');
             }
         });
@@ -53,8 +54,31 @@ bot.on('message', function (user, userID, channelID, message, evt) {
             switch(cmd) {
                 case 'goto':
                     var targetChannelName = args[1];
-                    enterVoiceChannel(bot, channelID, targetChannelName, function () {
-                    console.log('Joined ' + targetChannelName);
+                    enterVoiceChannel(bot, channelID, targetChannelName, function (error, events) {
+                        if (!error) {
+                            logger.info('Joined ' + targetChannelName);
+                        } else {
+                            logger.info('Failed to join ' + targetChannelName);
+                        }
+                    });
+                    break;
+                case 'play':
+                    var targetChannelName = args[1];
+                    var targetUrl = args[2];
+                    enterVoiceChannel(bot, channelID, targetChannelName, function(channelID) {
+                        // Get the audio context
+                        bot.getAudioContext(channelID, function(error, s) {
+                            //Once again, check to see if any errors exist
+                            if (error) return console.error(error);
+
+                            logger.info('Starting to play audio for ' + targetUrl + ' at ' + channelID);
+                            ytaStream(targetUrl).pipe(s, {end: false});
+
+                            //The stream fires `done` when it's got nothing else to send to Discord.
+                            s.on('done', function() {
+                               logger.info('Done playing audio for ' + targetUrl);
+                            });
+                        });
                     });
                     break;
                 case 'hi':
@@ -63,6 +87,7 @@ bot.on('message', function (user, userID, channelID, message, evt) {
                         message: "Hello :pizza:",
                         typing: true
                     })
+                    break;
             // Just add any case commands if you want to..
             }
         }
@@ -80,7 +105,13 @@ var enterVoiceChannel = function (targetBot,
     }
 
     if (destinationChannelID != null) {
-        targetBot.joinVoiceChannel(destinationChannelID, callback);
+        targetBot.joinVoiceChannel(destinationChannelID, function (error, events) {
+
+            //Check to see if any errors happen while joining.
+            if (error) return console.error(error);
+
+            callback(destinationChannelID);
+        });
     }
 }
 
